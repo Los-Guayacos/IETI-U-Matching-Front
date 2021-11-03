3de15929719e176c8551fde77dd512f2a608dc8a
jacepeda <jacepeda@indracompany.com>
2021-11-03 17:51:05 -0500

    private routes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
-import { BrowserRouter, Switch, Route, useHistory } from "react-router-dom";
-import { Home, Login, Register, Profile } from "./screens/index";
-import PrivateRoute from "./navigation/PrivateRoute";
-import { useEffect } from "react";
-import { useState } from "react";
-import UserContext from "./persistence/UserContext";
-import firebase from "firebase";
-import firebaseConfig from "./firebase/firebaseConfig"
-import Matching from "./screens/card/Matching";
-
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route, useHistory } from "react-router-dom"
+import { Home, Login, Register, Profile, Match, FilterSelection } from "./screens/index"
+import { NavBar } from './components/index';
+import PrivateRoute from './navigation/PrivateRoute';
+import firebase from "firebase"
+import { useEffect } from 'react';
+import { useState } from 'react';
+import UserContext from "./persistence/UserContext"
+import { firebaseConfig } from "./firebase/firebaseConfig"
 
 function App() {
-  const [userId, setUserId] = useState();
 
-  useEffect(() => {
-    if (!firebase.apps.length) {
-      firebase.initializeApp(firebaseConfig);
-    }
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        setUserId(user.uid);
-      }
-    });
-  }, []);
+    const [userId, setUserId] = useState();
 
-  return (
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                setUserId(user.uid);
+            }
+        })
+    }, [])
 
-    <UserContext.Provider value={userId}>
-      <BrowserRouter>
-        <Switch>
-          <PrivateRoute exact path="/profile" component={Profile} />
-          <PrivateRoute exact path="/matching" component={Matching} />
-          <PrivateRoute exact path="/" component={Home} />
-        </Switch>
-      </BrowserRouter>
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider value={userId}>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/login" component={Login} />
+                    <PrivateRoute exact path="/chat" component={Match} />
+                    <Route exact path="/register" component={Register} />
+                    <PrivateRoute exact path="/" component={Home} />
+                    <PrivateRoute exact path="/profile" component={Profile} />
+                    <PrivateRoute exact path="/filter" component={FilterSelection} />
+                </Switch>
+            </BrowserRouter>
+        </UserContext.Provider>
+    );
 }
-/*<Route exact path="/login" component={Login} />
-          <PrivateRoute exact path="/chat" component={Match} />
-          <Route exact path="/register" component={Register} /> */
+
 export default App;
